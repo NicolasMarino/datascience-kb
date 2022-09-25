51b491da5c573cef818fd864eb28fe1d76ab8404
Nicolás Marino <nicolasmarino.17@gmail.com>
2022-09-25 16:34:16 -0300

    Added new data.

---
+++ machine-learning/code/price_predictions.py
@@ -0,0 +1,26 @@
+import os
+import tarfile
+from six.moves import urllib
+import pandas as pd
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+ if not os.path.isdir(housing_path):
+     os.makedirs(housing_path)
+ tgz_path = os.path.join(housing_path, "housing.tgz")
+ urllib.request.urlretrieve(housing_url, tgz_path)
+ housing_tgz = tarfile.open(tgz_path)
+ housing_tgz.extractall(path=housing_path)
+ housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+ csv_path = os.path.join(housing_path, "housing.csv")
+ return pd.read_csv(csv_path)
+
+fetch_housing_data()
+housing = load_housing_data()
+housing.head()
